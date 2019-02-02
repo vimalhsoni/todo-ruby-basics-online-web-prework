505c226f1347ce9dac7ea9773f9d547628a3996a
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-02 01:44:52 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
-def division(num1, num2)
- 
+def division(num1 = 42, num2 = 7)
+ return num1/num2
 end
 
 def assign_variable(value)
-  
+value = 'Bob'
 end
 
-def argue
-  
+def argue(phrase = 'Nice')
+return phrase
 end
 
-def greeting
+def greeting(greeting,name)
 
 end
 
-def return_a_value
-
+def return_a_value (phrase = 'Nice')
+return phrase
 end
 
 def last_evaluated_value
-
+  putting_for_test = "Hope this Works"
+    return "expert"
+    "Guy Fieri"
 end
 
-def pizza_party
-
+def pizza_party(ingredients = 'cheese')
+  return ingredients
 end
